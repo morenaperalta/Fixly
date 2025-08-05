ad64395a4affd747a5bfe5f7bf08d9dd86395925
More-Pe <peraltamorena.va@gmail.com>
2025-08-05 14:11:49 +0200

    chore: first commit

---
+++ src/main/java/com/femcoders/fixly/FixlyApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.fixly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/fixly/FixlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.fixly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
