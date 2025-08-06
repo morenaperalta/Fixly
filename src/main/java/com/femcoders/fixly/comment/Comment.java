d66856791f9499345d1e15d478b9e8d0a47f6dff
More-Pe <peraltamorena.va@gmail.com>
2025-08-06 16:48:11 +0200

    fix(comment): add validations

--- src/main/java/com/femcoders/fixly/comment/Comment.java
+++ src/main/java/com/femcoders/fixly/comment/Comment.java
@@ -2,8 +2,11 @@ package com.femcoders.fixly.comment;
 
 import com.femcoders.fixly.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -12,13 +15,20 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    @NotBlank
+    @Size(min = 6)
+    @Column(nullable = false)
     public String content;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     public CommentType commentType;
 
+    @CreationTimestamp
     public LocalDateTime createdAt;
 
     @ManyToOne()
