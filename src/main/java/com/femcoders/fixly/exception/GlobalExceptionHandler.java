bca270282c4526af32bff0966f114a6f3e68f220
More-Pe <peraltamorena.va@gmail.com>
2025-08-20 20:37:59 +0200

    feat(GlobalExceptionHandler): add handleBadCredentials method

--- src/main/java/com/femcoders/fixly/exception/GlobalExceptionHandler.java
+++ src/main/java/com/femcoders/fixly/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.femcoders.fixly.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,5 +25,10 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(status, exception.getMessage(), controllerPath);
 
         return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
     }
 }
