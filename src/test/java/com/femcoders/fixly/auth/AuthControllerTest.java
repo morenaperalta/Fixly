78e5ee4f9980674c10dec714a73823851041607a
More-Pe <peraltamorena.va@gmail.com>
2025-08-20 21:58:20 +0200

    feat(AuthControllerTest): add login_WhenValidCredentials_ReturnsJwtResponse, login_WhenInvalidRequest_ReturnsBadRequest and login_WhenInvalidCredentials_ReturnsUnauthorized methods

--- src/test/java/com/femcoders/fixly/auth/AuthControllerTest.java
+++ src/test/java/com/femcoders/fixly/auth/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.femcoders.fixly.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.femcoders.fixly.auth.dtos.JwtResponse;
+import com.femcoders.fixly.auth.dtos.LoginRequest;
 import com.femcoders.fixly.auth.dtos.RegistrationRequest;
 import com.femcoders.fixly.user.dtos.UserResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,29 +24,13 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @DisplayName("Tests for AuthController")
 class AuthControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private AuthService authService;
-    @Autowired
-    private ObjectMapper objectMapper;
-    private RegistrationRequest registrationRequest;
-    private UserResponse userResponse;
-
-    @BeforeEach
-    void setUp() {
-        registrationRequest = new RegistrationRequest("User1", "user1@email.com", "Password123!", "FirstName", "LastName", "Company");
-        userResponse = new UserResponse("User1", "user1@email.com", "FirstName", "LastName", "Company");
-    }
 
     @TestConfiguration
     static class TestConfig {
@@ -54,6 +41,28 @@ class AuthControllerTest {
         }
     }
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private RegistrationRequest registrationRequest;
+    private UserResponse userResponse;
+    private LoginRequest loginRequest;
+    private JwtResponse jwtResponse;
+
+    @BeforeEach
+    void setUp() {
+        registrationRequest = new RegistrationRequest("User1", "user1@email.com", "Password123!", "FirstName", "LastName", "Company");
+        userResponse = new UserResponse ("User1", "user1@email.com", "FirstName", "LastName", "Company");
+        loginRequest = new LoginRequest("User1", "Password123!");
+        jwtResponse = new JwtResponse("token");
+    }
+
     @Nested
     @DisplayName("Register")
     class RegisterTests {
@@ -62,7 +71,12 @@ class AuthControllerTest {
         void register_WhenValidRequest_ReturnsCreatedUser() throws Exception {
             when(authService.register(any(RegistrationRequest.class))).thenReturn(userResponse);
 
-            mockMvc.perform(post("/api/auth/register").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(registrationRequest))).andExpect(status().isCreated()).andExpect(jsonPath("$.username").value("User1")).andExpect(jsonPath("$.email").value("user1@email.com"));
+            mockMvc.perform(post("/api/auth/register")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(registrationRequest)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.username").value("User1"))
+                    .andExpect(jsonPath("$.email").value("user1@email.com"));
         }
 
         @Test
@@ -70,7 +84,48 @@ class AuthControllerTest {
         void register_WhenInvalidRequest_ReturnsBadRequest() throws Exception {
             RegistrationRequest invalidRequest = new RegistrationRequest("", "", "", "", "", "");
 
-            mockMvc.perform(post("/api/auth/register").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(invalidRequest))).andExpect(status().isBadRequest());
+            mockMvc.perform(post("/api/auth/register")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(invalidRequest)))
+                    .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("Login")
+    class LoginTests {
+        @Test
+        @DisplayName("Should return JWT token when login is successful")
+        void login_WhenValidCredentials_ReturnsJwtResponse() throws Exception {
+            when(authService.login(any(LoginRequest.class))).thenReturn(jwtResponse);
+
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(loginRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.token").value("token"));
+        }
+
+        @Test
+        @DisplayName("Should return 400 when login request is invalid")
+        void login_WhenInvalidRequest_ReturnsBadRequest() throws Exception {
+            LoginRequest invalidLogin = new LoginRequest("", "");
+
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(invalidLogin)))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Should return 401 when credentials are invalid")
+        void login_WhenInvalidCredentials_ReturnsUnauthorized() throws Exception {
+            when(authService.login(any(LoginRequest.class))).thenThrow(new BadCredentialsException("Invalid credentials"));
+
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(loginRequest)))
+                    .andExpect(status().isUnauthorized());
         }
     }
 }
