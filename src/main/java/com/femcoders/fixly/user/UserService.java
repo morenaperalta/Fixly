3e17ad9a45166a2bc235af7deeae3a0c16b3bf12
More-Pe <peraltamorena.va@gmail.com>
2025-08-23 00:51:20 +0200

    feat(UserService): add getAuthenticatedUser method

--- src/main/java/com/femcoders/fixly/user/UserService.java
+++ src/main/java/com/femcoders/fixly/user/UserService.java
@@ -5,6 +5,7 @@ import com.femcoders.fixly.shared.security.CustomUserDetails;
 import com.femcoders.fixly.user.dtos.AdminResponse;
 import com.femcoders.fixly.user.dtos.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,18 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws EntityNotFoundException {
         return userRepository.findByUsername(username)
                 .map(CustomUserDetails::new)
+                .orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), "username", username));
+    }
+
+    public User getAuthenticatedUser() {
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String username = userDetails.getUsername();
+
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), "username", username));
     }
 }
